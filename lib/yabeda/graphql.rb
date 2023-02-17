917b1ed04ecbeeb2b95c06c65ffd02dcc372d320
Philip McGavin <pdsmcgavin@gmail.com>
2023-02-17 17:44:45 +0000

    Changed default buckets

--- lib/yabeda/graphql.rb
+++ lib/yabeda/graphql.rb
@@ -8,7 +8,7 @@ module Yabeda
     class Error < StandardError; end
 
     REQUEST_BUCKETS = [
-      0.001, 0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10,
+      0.01, 0.05, 0.1, 0.25, 0.5, 1, 5, 10, 30, 60
     ].freeze
 
     Yabeda.configure do
@@ -16,17 +16,17 @@ module Yabeda
 
       histogram :field_resolve_runtime, comment: "A histogram of field resolving time",
                 unit: :seconds, per: :field,
-                tags: %i[type field deprecated],
+                tags: %i[type field],
                 buckets: REQUEST_BUCKETS
 
       counter :fields_request_count, comment: "A counter for specific fields requests",
-              tags: %i[type field deprecated]
+              tags: %i[type field]
 
       counter :query_fields_count, comment: "A counter for query root fields",
-              tags: %i[name deprecated]
+              tags: %i[name]
 
       counter :mutation_fields_count, comment: "A counter for mutation root fields",
-              tags: %i[name deprecated]
+              tags: %i[name]
     end
 
     def self.use(schema)
